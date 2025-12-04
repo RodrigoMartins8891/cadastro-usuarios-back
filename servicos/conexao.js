ef91dcfc659b785d5a312a66714cbaaabf376705
Rodrigo Martins <rmartinsdeoliveira2@gmail.com>
2025-12-04 14:43:50 -0300

    Change MySQL connection environment variables
    Updated MySQL connection pool environment variables.

--- servicos/conexao.js
+++ servicos/conexao.js
@@ -1,14 +1,16 @@
+// pool.js
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: process.env.MYFQ_MYSQL_HOST,      // <--- MUDANÇA AQUI
+    user: process.env.MYFQ_MYSQL_USER,      // <--- MUDANÇA AQUI
+    password: process.env.MYFQ_MYSQL_PASSWORD, // <--- MUDANÇA AQUI
+    database: process.env.MYFQ_MYSQL_DATABASE, // <--- MUDANÇA AQUI
+    port: process.env.MYFQ_MYSQL_PORT        // <--- MUDANÇA AQUI
 });
 
 export default pool;
+
